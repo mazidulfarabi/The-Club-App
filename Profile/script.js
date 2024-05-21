b8aa2c2158bd4a5f821e256c4644e17219f33459
Md Mazidul Haque Farabi <mhfarabi09@gmail.com>
2024-05-21 12:36:19 +0600

    Update script.js

--- Profile/script.js
+++ Profile/script.js
@@ -1,12 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     var userData = JSON.parse(sessionStorage.getItem('userData'));
     if (userData) {
-        document.getElementById('userName').textContent = userData.name;
-        document.getElementById('userPost').textContent = userData.post;
         document.getElementById('userDesigns').textContent = userData.designs;
-        document.getElementById('userCaptions').textContent = userData.captions;
         // Display other fields as necessary
     } else {
-        window.location.href = 'login.html';
+        window.location.href = 'https://the-club-app.netlify.app/';
     }
 });
