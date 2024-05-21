cdcd62135a68b802058239e98a6396a4138dfc25
Md Mazidul Haque Farabi <mhfarabi09@gmail.com>
2024-05-21 15:30:56 +0600

    tasks

---
+++ Tasks/script.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var userData = JSON.parse(sessionStorage.getItem('userData'));
+    if (userData) {
+        document.getElementById('userPhone').value = userData.phone;
+        // Display other fields as necessary
+    } else {
+        window.location.href = 'https://the-club-app.netlify.app/';
+    }
+
+    var url = "https://sheets.googleapis.com/v4/spreadsheets/1zTiZgTCnsMQASTsBvnIteXz63Hyt6DphPNKm60Rlces/values/members?alt=json&key=AIzaSyCJ4u6ZfdiiEAVh5p0bv4eoRIoYKGCTyR0";
+
+    $.getJSON(url, function(data) {
+
+
+        for (var i = 1; i < data.values.length; i++) {
+
+            document.getElementById('taskType').textContent = data.values[i][0];
+            document.getElementById('taskTitle').textContent = data.values[i][1];
+            document.getElementById('taskImage').src = data.values[i][2];
+            document.getElementById('taskDetails').textContent = data.values[i][3];
+            document.getElementById('postedByName').textContent = data.values[i][4];
+            document.getElementById('postedByImage').src = data.values[i][5];
+            document.getElementById('postedByPost').textContent = data.values[i][6];
+                break;
+        }
+            window.location.href = 'https://the-club-app.netlify.app/profile/';
+
+
+    });
+})
